c618b2eb8ec2d491ff37efbe292da04867acabf9
Marta Pascual Estarellas <estaremp@Randa.local>
2018-11-15 11:45:59 +0900

    Tests 1.1 to 2.3 successfully passed

--- Cryptosystems/T2018_Practica2_Skeleton.py
+++ Cryptosystems/T2018_Practica2_Skeleton.py
@@ -62,14 +62,17 @@ def uoc_ext_a5_pseudo_random_gen(params_pol_0, params_pol_1, params_pol_2, clock
 
     # --- IMPLEMENTATION GOES HERE ---
 
+    #slice input parameters into LSFR states
     state_1 = params_pol_0[1]
     state_2 = params_pol_1[1]
     state_3 = params_pol_2[1]
     
+    #initial bit index of each LSR
     gates_1 = len(state_1)
     gates_2 = len(state_2)
     gates_3 = len(state_3)
     
+    #LSFR polynomial
     conn_1 = params_pol_0[0]
     conn_2 = params_pol_1[0]
     conn_3 = params_pol_2[0]
@@ -78,15 +81,20 @@ def uoc_ext_a5_pseudo_random_gen(params_pol_0, params_pol_1, params_pol_2, clock
         print("ERROR: Clocking bits are not valid for this set up.")
     
     i = 0
+    #loop for as many needed bits in the sequence
     while (i<output_bits):
         
+        #xor last bits of the state of each LSFR
         z = state_1[0]^state_2[0]^state_3[0]
         sequence.append(z)
         
         #check state of clocking bits
         clck = [state_1[-clocking_bits[0]-1],state_2[-clocking_bits[1]-1],state_3[-clocking_bits[2]-1]]
+        
+        #find what the majority says
         vote = max(set(clck), key = clck.count)
 
+        #when part of the majority, displace one bit the LSFR state
         if clck[0]==vote:
             state_1.insert(gates_1,reduce(lambda a,b: a^b,[aa*bb for aa,bb in zip(state_1,conn_1)]))
             state_1.pop(0)
@@ -120,10 +128,12 @@ def uoc_a5_cipher(initial_state_0, initial_state_1, initial_state_2, message, mo
 
     # --- IMPLEMENTATION GOES HERE ---
     
+    #define the polynomial list
     conn_LFSR1 = [0]*19
     conn_LFSR2 = [0]*22
     conn_LFSR3 = [0]*23
     
+    #define coefficients for each LFSR
     conn_LFSR1[-19]=1
     conn_LFSR1[-18]=1
     conn_LFSR1[-17]=1
@@ -137,10 +147,12 @@ def uoc_a5_cipher(initial_state_0, initial_state_1, initial_state_2, message, mo
     conn_LFSR3[-21]=1
     conn_LFSR3[-8]=1
     
+    #put them together
     params_LFSR1 = [conn_LFSR1,initial_state_0]
     params_LFSR2 = [conn_LFSR2,initial_state_1]
     params_LFSR3 = [conn_LFSR3,initial_state_2]
     
+    #define clocking bits indices
     clck_A = [8,10,10]
     
     #encode
@@ -149,13 +161,21 @@ def uoc_a5_cipher(initial_state_0, initial_state_1, initial_state_2, message, mo
         msg_bin = [int(m) for m in bin(int.from_bytes(message.encode(),'big'))[2:]]
         msg_bin.insert(0,0)
         
+        #generate a5 sequence
         generated_seq = uoc_ext_a5_pseudo_random_gen(params_LFSR1, params_LFSR2, params_LFSR3, clck_A, len(msg_bin))
+        
+        #xor seq and message to encrypt and parse it to string for output
         output = ''.join(str(m) for m in [aa^bb for aa,bb in zip(generated_seq,msg_bin)])
 
     #decode
     if mode==1:
+        #generate a5 sequence
         generated_seq = uoc_ext_a5_pseudo_random_gen(params_LFSR1, params_LFSR2, params_LFSR3, clck_A, len(message))
+        
+        #xor seq and message to decrypt
         decoded_msg = int(''.join(str(m) for m in [aa^bb for aa,bb in zip(generated_seq,[int(s) for s in message])]),2)
+        
+        #parse from int to bytes, and from bytes to an ascii string
         output = decoded_msg.to_bytes((decoded_msg.bit_length() + 7) // 8, 'big').decode()
 
     # --------------------------------
@@ -176,6 +196,15 @@ def uoc_aes(message, key):
 
     # --- IMPLEMENTATION GOES HERE ---
 
+    block_size=16
+    
+    #string of bits to bytes
+    k = int(key,2).to_bytes(2*block_size, 'big')
+    m = int(message,2).to_bytes(block_size, 'big')
+    
+    #encrypt
+    aes = AES.new(k,AES.MODE_ECB)
+    cipher_text = ''.join(str(c) for c in format(int.from_bytes(aes.encrypt(m),'big'),'0128b'))
 
     # --------------------------------
 
@@ -194,7 +223,8 @@ def uoc_g(message):
 
     # --- IMPLEMENTATION GOES HERE ---
 
-
+    output = (message + message)
+    
     # --------------------------------
 
     return output
@@ -213,8 +243,16 @@ def uoc_naive_padding(message, block_len):
     output = ""
 
     # --- IMPLEMENTATION GOES HERE ---
+    
+    padding = ''
+    size_msg = (len(message)*8)
 
-
+    #create string of 0's the size of the block-len minus the division remainder
+    if (size_msg % block_len != 0):
+        padding = ''.join('0' for i in range(block_len - size_msg % block_len))
+    
+    #concatenate message bits with padding list o 0's
+    output = ''.join(str(c) for c in format(int.from_bytes(message.encode(),'big'),'0'+str(size_msg)+'b')) + padding
 
     # --------------------------------
 
